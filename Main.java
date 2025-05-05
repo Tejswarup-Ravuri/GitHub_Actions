e78161b1fc8e5f93298f3daecbed33a87b8a9e93
Ravuri <travuri@teksystems.com>
2025-05-05 14:20:49 +0530

    updated main code

--- Main.java
+++ Main.java
@@ -3,9 +3,8 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-	    Scanner sc=new Scanner(System.in);
-	    int a=sc.nextInt();
-	    int b=sc.nextInt();
+	    int a=55;
+	    int b=0;
 	    try
 	    {
 	        int result=a/b;
