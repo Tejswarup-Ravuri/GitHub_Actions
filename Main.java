1a657c18da1edbe1b668e246cdca03f5e1fe1293
Tejswarup-Ravuri <101380213+Tejswarup-Ravuri@users.noreply.github.com>
2025-08-26 13:53:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,16 +5,6 @@ public class Main
 	{
 	    int a=55.00;
 	    int b=0;
-	    try
-	    {
-	        int result=a/b;
-	        System.out.println("The result is"+result);
-	    }
-	    catch (ArithmeticException ae) 
-	    {
-            System.out.println("Error: Division by zero is not allowed.");
-            System.out.println("Please enter a non-zero value for the denominator.");
-        }
-
+	    System.out.println(a/b):
 	}
 }
